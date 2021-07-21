22adb36c82f361fef0b21821e9039a944ce26113
SaegusaMayumi <69360824+SaegusaMayumi1234@users.noreply.github.com>
2021-07-21 02:52:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const keepAlive = require('./server');
+const fs = require('fs');
+const Discord = require('discord.js')
+const client = Discord.Client()
+
+client.prefix = 'g>';
+client.commands = new Discord.collection()
+
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./src/commands/${file}`);
+	client.commands.set(command.name, command);
+};
+
+for (const file of eventFiles) {
+	const event = require(`./src/events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	};
+};
+
+keepAlive();
+
+client.login(process.env['DISCORD_TOKEN']);
